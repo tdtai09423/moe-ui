ae6d6c3f4b7947b4e2597e47246d5663c1955995
c-tim.nguyen <c-tim.nguyen@avepoint.com>
2026-01-15 13:45:36 +0700

    Revert "Merge branch 'Tim' into 'main'"
    This reverts commit 4af2b15628b38a426d7e4b276b2229a490f4df42, reversing
    changes made to 090a3dbbd93baa2cb263f50b84a5983b2346ea96.

--- moe_student_ui/src/App.jsx
+++ moe_student_ui/src/App.jsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import { RouterProvider } from 'react-router-dom'; 
-import { ConfigProvider } from 'antd';
-import { router } from './routes/index'; 
-import './assets/styles/main.scss';
+import React from 'react'
+
 
 function App() {
+
+
   return (
-    <ConfigProvider theme={{ token: { fontFamily: 'Inter', colorPrimary: '#1e3a8a' } }}>
-       <RouterProvider router={router} />
-    </ConfigProvider>
-  );
+    <>
+      <div>Hello World</div>
+    </>
+  )
 }
 
-export default App;
+export default App
